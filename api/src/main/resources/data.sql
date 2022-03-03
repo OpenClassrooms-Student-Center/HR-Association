fa2b7d081d8fab9f28e498fd90cf80d4bc8c1b2c
Fride10 <98041221+Fride10@users.noreply.github.com>
2022-03-03 19:49:43 +0100

    Update data.sql

--- api/src/main/resources/data.sql
+++ api/src/main/resources/data.sql
@@ -1,14 +1,16 @@
-DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS Fans;
  
-CREATE TABLE employees (
+CREATE TABLE Fans (
   id INT AUTO_INCREMENT  PRIMARY KEY,
   first_name VARCHAR(250) NOT NULL,
   last_name VARCHAR(250) NOT NULL,
-  mail VARCHAR(250) NOT NULL,
+  statut VARCHAR(250) NOT NULL,
+  Team VARCHAR(250) NOT NULL,
   password VARCHAR(250) NOT NULL
 );
  
-INSERT INTO employees (first_name, last_name, mail, password) VALUES
-  ('Laurent', 'GINA', 'laurentgina@mail.com', 'laurent'),
-  ('Sophie', 'FONCEK', 'sophiefoncek@mail.com', 'sophie'),
-  ('Agathe', 'FEELING', 'agathefeeling@mail.com', 'agathe');
+INSERT INTO Fans (first_name, last_name, mail, password) VALUES
+  ('Claude', 'Esthelle', 'Président', 'Féminine', 'Presi'),
+  ('Carrel', 'Landry', 'Vice President', 'Masculine', 'VPresi'),
+  ('Chanelle', 'Mefovou', 'Joueuse', 'Féminine', 'Chacha'),
+  ('Nelson', 'Yota', 'Joueur', 'Masculine', 'NeyNey');
